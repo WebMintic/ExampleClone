6e556b246d3fb84c2583759b6285663240322fe2
eralmoba <eralmoba@hotmail.com>
2021-07-07 19:08:25 -0500

    first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+r
+Bapp/src/main/java/com/example/mobile/javamodulo2/MainActivity.java,4\4\44100085d0fdd9f0785ea22c1b40d78ffed5a9dc
---
+++ app/src/main/java/com/example/mobile/javamodulo2/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.mobile.javamodulo2.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mobile.javamodulo2.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
